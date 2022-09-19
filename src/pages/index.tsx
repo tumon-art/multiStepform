9eee4aaafa36e07998879a20d288292720b8872c
tumon <tumonkhn@gmail.com>
2022-09-20 04:43:02 +0600

    inin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.scss";
+import { Formik, Form, Field } from "formik";
 
 const Home: NextPage = () => {
   return (
@@ -9,6 +10,10 @@ const Home: NextPage = () => {
         <title>React Form</title>
         <meta name="description" content=" A Multi Step Form" />
         <link rel="icon" href="/favicon.ico" />
+
+        <main className={styles.main}>
+          <h1 className={styles.h1}> Fill The Form </h1>
+        </main>
       </Head>
     </div>
   );
